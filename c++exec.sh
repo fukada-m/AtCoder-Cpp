d9b17d70ee608d60d6c2a9a44fb99c329c85da66
yami yunon <fukada.m888@gmail.com>
2024-03-31 18:14:51 +0900

    outputから先に出力した

--- c++exec.sh
+++ c++exec.sh
@@ -3,12 +3,11 @@
 g++ "./src/${FILE}" && \
 
 echo $FILE "が実行された"
-echo "==============inputの内容====================" && \
-cat ./input/input1.txt && \
-echo "" && \
-
 echo "==============outputの内容===================" && \
 ./a.out < input/input1.txt && \
 echo "" && \
+
+echo "==============inputの内容====================" && \
+cat ./input/input1.txt && \
 
 rm ./a.out
