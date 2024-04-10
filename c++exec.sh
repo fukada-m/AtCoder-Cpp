1e6629d325f673cf70216806d460fd8428974040
yami yunon <fukada.m888@gmail.com>
2024-04-11 08:52:39 +0900

    ディレクトリ構造の変更 timeで実行時間を測定できるようにした

--- c++exec.sh
+++ c++exec.sh
@@ -5,7 +5,7 @@ g++ "./src/$file.cpp" -o "./src/a.out"
 
 echo $file.cpp "が実行された"
 echo "==============outputの内容==================="
-./src/a.out
+time ./src/a.out
 echo ""
 
 echo "==============inputの内容===================="
