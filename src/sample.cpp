8cb194c39d3dbb0d3c88434dca33e15fc7c1c2ab
yami yunon <fukada.m888@gmail.com>
2024-04-29 09:52:18 +0900

    こういうの合ったほうがいいかなって思った

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 #define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
 
 
 int main(){
-
+  ifstream inputFile("/root/cpp/input/input.txt");
+  cin.rdbuf(inputFile.rdbuf());
 
 }
