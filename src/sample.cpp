666797585eb71fc93a93dc23e5fa6c558e9534a5
yami yunon <fukada.m888@gmail.com>
2024-04-29 12:45:25 +0900

    隙間の追加

--- src/sample.cpp
+++ src/sample.cpp
@@ -4,8 +4,11 @@ using ll = long long;
 #define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
 
 
+
 int main(){
   ifstream inputFile("/root/cpp/input/input.txt");
   cin.rdbuf(inputFile.rdbuf());
 
+        
+  
 }
