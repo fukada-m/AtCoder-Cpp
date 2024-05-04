ce2890fd471ba138448963dd6aab521adb293b43
fukada-m <fukada.m8888@gmail.com>
2024-05-04 09:15:55 +0900

    typo

--- sh-script/sample.sh
+++ sh-script/sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file="sample"
 
-g++ "root/cpp/src/$file.cpp" -o ".root/cpp/src/a.out"
+g++ "/root/cpp/src/$file.cpp" -o "/root/cpp/src/a.out"
 ./src/a.out
-rm root/cpp/src/a.out
+rm /root/cpp/src/a.out
